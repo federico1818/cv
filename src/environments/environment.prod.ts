b81a6c0fd8b2d8550e73627edcc5417f5639df27
Federico Gonzalez <federico1818@gmail.com>
2019-12-28 00:33:15 -0300

    Add config firebase on env.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
-};
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyDkL1_QYMnJ7M9Z30KIm4-LYmd7Cr3DYJs",
+        authDomain: "cv1818.firebaseapp.com",
+        databaseURL: "https://cv1818.firebaseio.com",
+        projectId: "cv1818",
+        storageBucket: "cv1818.appspot.com",
+        messagingSenderId: "690820927671",
+        appId: "1:690820927671:web:5540c09951a12cdb3abd32",
+        measurementId: "G-RNENQ4P1X3"
+    }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyDkL1_QYMnJ7M9Z30KIm4-LYmd7Cr3DYJs",
+        authDomain: "cv1818.firebaseapp.com",
+        databaseURL: "https://cv1818.firebaseio.com",
+        projectId: "cv1818",
+        storageBucket: "cv1818.appspot.com",
+        messagingSenderId: "690820927671",
+        appId: "1:690820927671:web:5540c09951a12cdb3abd32",
+        measurementId: "G-RNENQ4P1X3"
+    }
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
