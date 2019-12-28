ff0e1ea80d00c22ad25b75403f095244c6875daf
Federico Gonzalez <federico1818@gmail.com>
2019-12-28 02:06:54 -0300

    Create dock, notification-bar and phone components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,17 @@ import { AngularFirestoreModule } from '@angular/fire/firestore'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 
-import { environment } from '../environments/environment'
+import { environment } from '../environments/environment';
+import { PhoneComponent } from './phone/phone.component';
+import { NotificationBarComponent } from './notification-bar/notification-bar.component';
+import { DockComponent } from './dock/dock.component'
 
 @NgModule({
     declarations: [
         AppComponent,
+        PhoneComponent,
+        NotificationBarComponent,
+        DockComponent,
     ],
     imports: [
         BrowserModule,
