05c8ffdfd9aff8778f2f1f17854b37f036240570
Federico González <federico1818@gmail.com>
2020-01-16 01:45:54 -0300

    Get a work by id from firebase

--- src/app/experience-detail/experience-detail.component.ts
+++ src/app/experience-detail/experience-detail.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
+import { AngularFirestore } from '@angular/fire/firestore'
+import { Observable } from 'rxjs'
 
 @Component({
-  selector: 'app-experience-detail',
-  templateUrl: './experience-detail.component.html',
-  styleUrls: ['./experience-detail.component.scss']
+    selector: 'app-experience-detail',
+    templateUrl: './experience-detail.component.html',
+    styleUrls: ['./experience-detail.component.scss']
 })
+
 export class ExperienceDetailComponent implements OnInit {
+    public work
 
-  constructor() { }
+    constructor(
+        private route: ActivatedRoute,
+        private db: AngularFirestore
+    ) {
+        const id = this.route.snapshot.paramMap.get('id')
+        this.work = this.db.doc<any>(`experience/${ id }`).valueChanges()
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
