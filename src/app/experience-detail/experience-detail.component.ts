5a79a7756e70edac5c320d2133b6b08d88b9f804
Federico González <federico1818@gmail.com>
2020-01-31 01:48:20 -0300

    Add projects to experience list

--- src/app/experience-detail/experience-detail.component.ts
+++ src/app/experience-detail/experience-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
 import { AngularFirestore } from '@angular/fire/firestore'
 import { Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
 
 @Component({
     selector: 'app-experience-detail',
@@ -11,13 +12,17 @@ import { Observable } from 'rxjs'
 
 export class ExperienceDetailComponent implements OnInit {
     public work
+    public projects
 
     constructor(
         private route: ActivatedRoute,
         private db: AngularFirestore
     ) {
         const id = this.route.snapshot.paramMap.get('id')
-        this.work = this.db.doc<any>(`experience/${ id }`).valueChanges()
+        let workRef = this.db.collection(`experience`).doc(id)
+        
+        this.work = workRef.valueChanges()
+        this.projects = workRef.collection(`projects`).valueChanges()
     }
 
     ngOnInit() {
