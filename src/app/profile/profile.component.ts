a9422db730da7ed855ba56bf8e94002e52c767ce
Federico González <federico1818@gmail.com>
2020-02-12 20:23:12 -0300

    Profile styles

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { AngularFirestore } from '@angular/fire/firestore'
+import { Observable } from 'rxjs'
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+    selector: 'app-profile',
+    templateUrl: './profile.component.html',
+    styleUrls: ['./profile.component.scss']
 })
+
 export class ProfileComponent implements OnInit {
+    public profile: Observable<any>
+    
+    constructor(
+        private db: AngularFirestore
+    ) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.getProfile()
+    }
+    
+    private getProfile(): void {
+        this.profile = this.db.doc('profile/1').valueChanges()
+    }
 
 }
